144bbb6207d810fffb5156327f41bd729589f11d
tranvy10122000 <tranvy10122000@gmail.com>
2025-05-27 15:39:32 +0700

    Vy commit 2

--- src/TestCase/TC2.java
+++ src/TestCase/TC2.java
@@ -3,6 +3,7 @@ package TestCase;
 public class TC2 {
     public static void main(String[] args) {
         System.out.println("Tesst tesst");
-        
+        System.out.println("Vy add lan 2");
+
     }
 }
